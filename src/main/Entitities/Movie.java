578881ac2007b4e0f0878be5ceb22a8338e53022
tonycituk <tonycituk@gmail.com>
2022-03-17 19:47:40 -0600

    New Project

---
+++ src/main/Entitities/Movie.java
@@ -0,0 +1,5 @@
+package main.Entitities;
+
+public class Movie {
+    
+}
---
+++ src/test/TestAlgorithms.java
@@ -0,0 +1,31 @@
+package test;
+
+/**
+ * TestAlgorithms
+ * 
+ * @author tonycituk
+ */
+public class TestAlgorithms {
+
+    public static void testInsertionSort(){
+
+    }
+    public static void testQuickSort(){
+        
+    }
+
+    public static void testRadixSort(){
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Testing sorting algorithms . . .");
+        
+        TestAlgorithms.testInsertionSort();
+        TestAlgorithms.testQuickSort();
+        TestAlgorithms.testRadixSort();
+
+        
+    }
+
+    
+}
